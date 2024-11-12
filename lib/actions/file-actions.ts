08f10d7830829857e1be5e128949791ddfb09633
Antonis Papaioannou <mrantonp@gmail.com>
2024-11-12 14:40:18 +0200

    Add FormattedDateTime component, implement file renaming functionality, and introduce Dialog component for enhanced UI interactions

--- lib/actions/file-actions.ts
+++ lib/actions/file-actions.ts
@@ -91,3 +91,26 @@ export const getFiles = async () => {
   }
  
 };
+
+
+export const renameFile = async ({ fileId, name, extension, path }: RenameFileProps) => {
+  const { databases } = await createAdminClient();
+
+  try {
+    const newName = `${name}.${extension}`;
+    const updateFile = await databases.updateDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      fileId,
+      { name: newName },
+    );
+
+    revalidatePath(path);
+
+    return parseStringify(updateFile);
+
+  } catch (error) {
+    handleError(error, "Failed to rename file");
+    
+  }
+};
