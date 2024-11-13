2420dc238f248bc2c08656607ae05b75d3d5b2e7
Antonis Papaioannou <mrantonp@gmail.com>
2024-11-13 17:39:05 +0200

    Enhance file retrieval logic by adding search and sorting capabilities, and update getFiles function to accept parameters for file types, search text, and sorting options.

--- lib/actions/file-actions.ts
+++ lib/actions/file-actions.ts
@@ -55,7 +55,7 @@ export const uploadFile = async ({ file, ownerId, accountId, path }: UploadFileP
 };
 
 
-const createQueries = (currentUser: Models.Document) => {
+const createQueries = (currentUser: Models.Document, types: string[], searchText: string, sort: string, limit?: number) => {
   const queries = [
     Query.or ([
       Query.equal("owner", [currentUser.$id]), 
@@ -63,10 +63,19 @@ const createQueries = (currentUser: Models.Document) => {
     ]),
   ];
 
+  if (types.length > 0) queries.push(Query.equal("type", types));
+  if (searchText) queries.push(Query.contains("name", searchText));
+  if (limit) queries.push(Query.limit(limit));
+
+  const [sortBy, orderBy] = sort.split("-");
+
+  queries.push(orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy));
+    
+
   return queries;
 }
 
-export const getFiles = async () => {
+export const getFiles = async ({ types = [], searchText = "" , sort = "$createdAt-desc", limit }: GetFilesProps) => {
   const { databases } = await createAdminClient();
 
   try {
@@ -76,7 +85,7 @@ export const getFiles = async () => {
       throw new Error("User not found");
     }
 
-    const queries = createQueries(currentUser);
+    const queries = createQueries(currentUser, types, searchText, sort, limit);
 
     const files = await databases.listDocuments(
       appwriteConfig.databaseId,
